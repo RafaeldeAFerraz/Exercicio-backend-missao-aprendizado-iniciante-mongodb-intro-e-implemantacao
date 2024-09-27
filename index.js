9f019cb04f2fa3dedec2821bf419b050f7caac72
Rafael Ferraz <140864570+RafaeldeAFerraz@users.noreply.github.com>
2024-09-27 00:53:00 +0000

    Conexão com banco de dados

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
 
-const dbUrl = 'mongodb+srv://admin:KJsim29sjakjm437A@cluster0.mxtti.mongodb.net'
-const dbName = 'exercicio-mongodb-intro-e-implementacao'
+const dbUrl = 'mongodb+srv://admin:KJsim29sjakjm437A@cluster0.m8eas.mongodb.net'
 
-const app = express()
+const dbName = 'exercicio-livro'
 
+async function main() {
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+    const client = new MongoClient(dbUrl)
+    console.log('Conectando ao banco de dados...')
+    await client.connect()
+    console.log('Banco de dados conectado com sucesso!')
 
-app.listen(3000)
+    const app = express()
+
+    app.get('/', function (req, res) {
+        res.send('Hello World')
+    })
+
+    app.listen(3000)
+}
+
+main()
