f7bfac8de9dffba0adf1947905628edb03469697
Rafael Ferraz <140864570+RafaeldeAFerraz@users.noreply.github.com>
2024-09-27 00:04:36 +0000

    Instalação do MongoDB e preparação de acesso ao banco de dados.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+
+const dbUrl = 'mongodb+srv://admin:KJsim29sjakjm437A@cluster0.mxtti.mongodb.net'
+const dbName = 'exercicio-mongodb-intro-e-implementacao'
+
 const app = express()
+
 
 app.get('/', function (req, res) {
   res.send('Hello World')
